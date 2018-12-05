b41911121aa5e51640229ee72be14ceace5681d1
Xneg <xnegxneg@gmail.com>
2018-12-05 22:10:55 +0300

    Day 5 code lines optimization

--- src/App/Day05.fs
+++ src/App/Day05.fs
@@ -3,19 +3,21 @@ module Day05
 open Utilities;
 
 let check a acc =
-    let shouldDestroy (a : char) (b : char) = abs(int a - int b) = 32
+    let shouldDestroy a b = abs(int a - int b) = 32
     match acc with
     | [] -> [a]
-    | [b] -> if shouldDestroy a b then [] else [a;b]
     | b::tail -> if shouldDestroy a b then tail else a::b::tail
 
 let processData data =
     let rec recProcess acc data =
         match data with
         | [] -> acc
-        | [a] -> check a acc
         | a::tail -> recProcess (check a acc) tail 
-    recProcess [] data |> List.rev   
+    recProcess [] data |> List.length  
+
+let removedChars = List.map2 (fun x y -> (x, y)) [ 'a' .. 'z' ] [ 'A' .. 'Z' ]
+
+let removeChar (a, b) = List.filter (fun x -> x <> a && x <> b)
 
 let fileName = @"./data/05.txt"
 
@@ -28,19 +30,10 @@ let data =
 let firstPuzzle() =
     data
     |> processData
-    |> List.length
     |> printfn "%i"
-
-let removedChars =
-    let removedCharsSmall = [ 'a' .. 'z' ]
-    let removedCharsBig = [ 'A' .. 'Z' ]
-    List.map2 (fun x y -> (x, y)) removedCharsSmall removedCharsBig
-
-let removeChar (a, b) data =
-    data |> List.filter (fun x -> x <> a && x <> b)
 
 let secondPuzzle() =
     [for c in removedChars -> 
-        data |> removeChar c |> processData |> List.length] 
+        data |> removeChar c |> processData] 
     |> List.min
     |> printfn "%i"    
