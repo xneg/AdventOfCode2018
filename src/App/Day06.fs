54dee9b3140e829092a7b3ca65d3ff12ab1dbcb1
Evgeny Bikkinin <e.bikkinin@dodopizza.com>
2018-12-06 18:46:29 +0300

    Day 6. Can be a little bit improved

--- src/App/Day06.fs
+++ src/App/Day06.fs
@@ -1,15 +1,72 @@
 module Day06
 
-type Point = int * int
+let outerBounds data = 
+    let x1 = data |> List.minBy (fst) |> fst
+    let y1 = data |> List.minBy (snd) |> snd
+    let x2 = data |> List.maxBy (fst) |> fst
+    let y2 = data |> List.maxBy (snd) |> snd
+    (x1 - 1, y1 - 1, x2 + 1, y2 + 1)
 
-let input = [(1, 1);(1, 6);(8, 3);(3, 4);(5, 5);(8, 9)]
+let manhatten a b = abs(fst a - fst b) + abs(snd a - snd b)
 
-let bounds (data : Point list) = 
-    let left = data |> List.minBy (fst) |> fst
-    let top = data |> List.minBy (snd) |> snd
-    let width = (data |> List.maxBy (fst) |> fst) - left
-    let height = (data |> List.maxBy (snd) |> snd) - top
-    ((left, top), (width, height))
+let getNearestId pos points =
+    let [x; y] =
+        points 
+        |> Seq.mapi (fun i x -> (i, manhatten pos x))
+        |> Seq.sortBy snd
+        |> Seq.take 2    
+        |> Seq.toList
+    if snd x <> snd y then fst x else -1
 
-input |> bounds
+let getTotal pos points = points |> Seq.map (fun x -> manhatten pos x) |> Seq.sum
 
+let calcField (x1, y1, x2, y2, data) f =
+    [for x in x1 .. x2 do
+        for y in y1 .. y2 ->
+            ((x, y), f (x, y) data)]
+
+let firstPuzzleResult data =
+    let x1, y1, x2, y2 = outerBounds data
+    let field = calcField (x1, y1, x2, y2, data) getNearestId
+
+    let excluded =
+        field 
+        |> List.filter (fun ((x, y), _) -> x = x1 || x = x2 || y = y1 || y = y2) 
+        |> List.map snd
+        |> List.distinct
+    
+    field 
+        |> List.map (snd) 
+        |> List.groupBy (fun x -> x) 
+        |> List.map (fun x -> (fst x, snd x |> List.sumBy (fun _ -> 1)))
+        |> List.filter (fun x -> fst x <> -1)
+        |> List.filter (fun x -> not (List.contains (fst x) excluded))
+        |> List.sortByDescending (snd)
+        |> List.head |> snd
+
+let secondPuzzleResult limit data =
+    let left, top, width, height = outerBounds data
+    let field = calcField (left, top, width, height, data) getTotal
+
+    field |> List.map snd |> List.filter (fun x -> x < limit) |> List.sumBy (fun x -> 1)    
+
+let processFile (filePath : string) =
+        seq {
+            use fileReader = new System.IO.StreamReader(filePath)
+
+            while not fileReader.EndOfStream do
+                let line = fileReader.ReadLine()
+                yield line.Split([|'\t';' ';','|])
+        }
+
+let fileName = @"./data/06.txt"
+
+let data = 
+    fileName 
+    |> processFile 
+    |> Seq.map (fun x -> (int x.[0], int x.[2]))
+    |> Seq.toList
+
+let firstPuzzle() = data |> firstPuzzleResult
+
+let secondPuzzle() = data |> secondPuzzleResult 10000
